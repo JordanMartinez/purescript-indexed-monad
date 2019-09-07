342b8c1e694235101d158ecb0278e5dbdeeac0a3
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-09-07 13:58:45 -0700

    Allow ipure to change transition

--- src/Control/Applicative/Indexed.purs
+++ src/Control/Applicative/Indexed.purs
@@ -11,12 +11,12 @@ import Control.Apply.Indexed
 import Control.ValidTransition (class ValidTransition)
 
 class IxApply m ⇐ IxApplicative m where
-  ipure ∷ ∀ a x. ValidTransition x x ⇒ a → m x x a
+  ipure ∷ ∀ a x y. ValidTransition x y ⇒ a → m x y a
 
-iwhen ∷ ∀ m x. ValidTransition x x ⇒ IxApplicative m ⇒ Boolean → m x x Unit → m x x Unit
+iwhen ∷ ∀ m x y. ValidTransition x y ⇒ IxApplicative m ⇒ Boolean → m x y Unit → m x y Unit
 iwhen true m = m
 iwhen false _ = ipure unit
 
-iunless ∷ ∀ m x. ValidTransition x x ⇒ IxApplicative m ⇒ Boolean → m x x Unit → m x x Unit
+iunless ∷ ∀ m x y. ValidTransition x y ⇒ IxApplicative m ⇒ Boolean → m x y Unit → m x y Unit
 iunless false m = m
 iunless true _ = ipure unit
